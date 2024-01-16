71ff436be46b5ff57c2f94afe742e9590f56464a
Hendrick_Samuel <samuel@certinergie.be>
2024-01-16 23:41:03 +0100

    Add skills and fix layout

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -9,7 +9,7 @@ export const RESUME_DATA = {
   about:
     "Frontend Analyst Developer with 2 years of experience in developing and maintaining web applications.",
   summary:
-    "As I started as a Backend Developer and then moved to become a Frontend Developer, I have a good understanding of the full stack. This aspect helps me on a daily basis to better understand the needs of the project and to be able to communicate with the other team members.",
+    "I am a Frontend Developer that started as a Backend Developer. I therefore have a good understanding of the full stack. This aspect helps me on a daily basis to better understand the needs of the project and to be able to communicate with the other team members.",
   avatarUrl: "/profile-alt.png",
   // personalWebsiteUrl: "https://jarocki.me",
   contact: {
@@ -49,7 +49,15 @@ export const RESUME_DATA = {
         "Created a custom solution for notaries and real estate agencies to manage and publish their real estate ads. Technologies: Turborepo, React, TypeScript, Chakra-UI, Next.js",
     },
   ],
-  skills: ["JavaScript", "TypeScript", "React/Next.js", "Turborepo", "Git"],
+  skills: [
+    "JavaScript",
+    "TypeScript",
+    "React/Next.js",
+    "Turborepo",
+    "Git",
+    "Github Actions",
+    "Meilisearch",
+  ],
   projects: [
     {
       title: "MagicImmo",
